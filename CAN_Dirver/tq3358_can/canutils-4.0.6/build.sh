6eaf28549f240a18b738181e40b02d40f65881aa
FeiPingWang <wfp19920821@163.com>
2016-06-06 10:56:15 +0800

    CanDirver c file tidy in 6.6

--- CAN_Dirver/tq3358_can/canutils-4.0.6/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export PKG_CONFIG_PATH=/opt/EmbedSky/4.4.6/arm-embedsky-linux-gnueabi/embedsky/usr/lib/pkgconfig/
-./configure --host=arm-linux --prefix=`pwd`/../__install --enable-debug &&
-make &&
-make install
